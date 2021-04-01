22fd3f7676a4bdd223aacc1792b2e325a2dd2b53
stuwilmur <stuwilmur@users.noreply.github.com>
2021-04-01 10:03:33 +0100

    Minor fixes to deflator in v1.6

--- deflator.js
+++ deflator.js
@@ -17,7 +17,7 @@ function initDeflator(_gdpdef){
 function deflate(_amount, _yearIn, _yearOut = 2010){
     var defIn   = gdpDefByYear.filter(d=> d.key == _yearIn)
     var defOut  = gdpDefByYear.filter(d=> d.key == _yearOut)
-    if (defIn === undefined || defOut === undefined){
+    if (defIn === undefined || _amount < 0 || defOut === undefined){
         return NaN
     }
     defIn = defIn[0].values[0].GDPDEF;
@@ -29,5 +29,5 @@ function updateDeflator(){
     var amount = +document.getElementById("deflatorAmount").value;
     var year = document.getElementById("deflatorYear").value;
     var result = deflate(amount, year);
-    document.getElementById("deflatorResult").value = result.toFixed(4);
+    document.getElementById("deflatorResult").value = result.toFixed(2);
 }
