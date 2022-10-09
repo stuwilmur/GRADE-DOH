ad7c111df0708be6203ce46e9b82a14e0f45b90e
stuwilmur <stuwilmur@users.noreply.github.com>
2022-10-09 18:32:42 +0100

    Update comments
    Comments updated

--- projection.js
+++ projection.js
@@ -112,7 +112,7 @@ function getProjectionData(_firstyear, _country, _outcome, _years_to_return, _re
         {
             // Get a blank set of additional results (set to NaN)
             datarow.additional = computeAdditionalResults(_country, y, _outcome, NaN, NaN)
-            // Try and compute the governance despite the error
+            // Try and compute the governance despite the error being present
             datarow.gov = computegovernance(_country, y, _governance, grpc)
         }
 
