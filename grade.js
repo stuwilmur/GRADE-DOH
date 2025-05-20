09c8cd7cb758ce9a4113128e5724a65f2baaf4d4
stuwilmur <stuwilmur@users.noreply.github.com>
2025-05-20 17:22:32 +0100

    Bump minor version

--- grade.js
+++ grade.js
@@ -1,5 +1,5 @@
-var version = "GRADE v3.16.2"
-var date = "2025/05/09"
+var version = "GRADE v3.17.0"
+var date = "2025/05/20"
 var subheight = 100;
 var legendCells = 11;
 var transitionTime = 500;
