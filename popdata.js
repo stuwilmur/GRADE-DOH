5ff46a19b07bbb3473df203c1038a8b5075edccb
stuwilmur <stuwilmur@users.noreply.github.com>
2025-12-30 17:00:04 +0000

    Run prettier

--- popdata.js
+++ popdata.js
@@ -1,88 +1,86 @@
 class PopData {
-    constructor()
-    {}
-    nestdata(_csvdata) {
-        this.nesteddata = d3.nest()
-            .key(function (d) {
-                return d.ISO;
-            })
-            .key(function (d) {
-                return d.year;
-            })
-            .entries(_csvdata);
-        
-        this.firstyear = d3.min(_csvdata, d => d.year);
-        this.lastyear = d3.max(_csvdata, d => d.year);
-    }
+  constructor() {}
+  nestdata(_csvdata) {
+    this.nesteddata = d3
+      .nest()
+      .key(function (d) {
+        return d.ISO;
+      })
+      .key(function (d) {
+        return d.year;
+      })
+      .entries(_csvdata);
 
-    getvalue(_iso, _year, _var, interp=false) {
-        var row = this.getrow(_iso, _year);
-        if (!row) 
-        {
-            return NaN;
-        } 
-        else 
-        {
-            if (isNaN(row[_var]) || row[_var] === 0) 
-            {
-                if (interp)
-                {
-                    var series = this.getseries(_iso, _var);
-                    var seriesFiltered = removeblanks(series);
-                    var f = linearInterpolator(seriesFiltered, false);
-                    return f(_year);
-                }
-                else 
-                {
-                    return NaN;
-                }
-            }
-            else 
-            {
-                return row[_var];
-            }
+    this.firstyear = d3.min(_csvdata, (d) => d.year);
+    this.lastyear = d3.max(_csvdata, (d) => d.year);
+  }
+
+  getvalue(_iso, _year, _var, interp = false) {
+    var row = this.getrow(_iso, _year);
+    if (!row) {
+      return NaN;
+    } else {
+      if (isNaN(row[_var]) || row[_var] === 0) {
+        if (interp) {
+          var series = this.getseries(_iso, _var);
+          var seriesFiltered = removeblanks(series);
+          var f = linearInterpolator(seriesFiltered, false);
+          return f(_year);
+        } else {
+          return NaN;
         }
-    }
-    
-    getstring(_iso, _year, _var){
-        var row = this.getrow(_iso, _year);
-        if (!row)
-            {
-                return NaN;
-            }
+      } else {
         return row[_var];
+      }
     }
+  }
 
-    getrow(_iso, _year=-1) {
-        var objIso = this.nesteddata.filter(function (d) {
-            return d.key == _iso
+  getstring(_iso, _year, _var) {
+    var row = this.getrow(_iso, _year);
+    if (!row) {
+      return NaN;
+    }
+    return row[_var];
+  }
+
+  getrow(_iso, _year = -1) {
+    var objIso = this.nesteddata.filter(function (d) {
+      return d.key == _iso;
+    });
+    if (objIso.length > 0) {
+      if (_year == -1) {
+        return objIso[0].values.map(function (d) {
+          return d.values[0];
         });
-        if (objIso.length > 0) {
-            if (_year == -1) {
-                return objIso[0].values.map(function(d){return d.values[0]});
-            } else {
-                var objIsoYear = objIso[0].values.filter(function (d) {
-                    return d.key == _year
-                })
-                if (objIsoYear.length > 0) {
-                    return objIsoYear[0].values[0];
-                }
-            }
+      } else {
+        var objIsoYear = objIso[0].values.filter(function (d) {
+          return d.key == _year;
+        });
+        if (objIsoYear.length > 0) {
+          return objIsoYear[0].values[0];
         }
+      }
     }
+  }
 
-    getseries(_iso, _var){
-        var series = this.getrow(_iso, -1);
-        if (!series) {return NaN};
-        return series.map(function(d){return [d.year, d[_var]];})
+  getseries(_iso, _var) {
+    var series = this.getrow(_iso, -1);
+    if (!series) {
+      return NaN;
     }
+    return series.map(function (d) {
+      return [d.year, d[_var]];
+    });
+  }
 
-    getcountries(){
-        return this.nesteddata.map(x=>x.key)
-    }
+  getcountries() {
+    return this.nesteddata.map((x) => x.key);
+  }
 }
 
-function removeblanks(_s){
-    // given array s remove NaNs and zeros
-    return _s.filter(function(d){return !isNaN(d[1]) && (d[1] !== 0)});
+function removeblanks(_s) {
+  // given array s remove NaNs and zeros
+  return _s.filter(function (d) {
+    return !isNaN(d[1]) && d[1] !== 0;
+  });
 }
--- tooltip.js
+++ tooltip.js
@@ -2,36 +2,38 @@
 
 d3.helper = {};
 
-d3.helper.tooltip = function(accessor, enabler){
-    return function(selection){
-        var tooltipDiv;
-        var bodyNode = d3.select('body').node();
-        selection.on("mouseover", function(d, i){
-            // Clean up lost tooltips
-            d3.select('body').selectAll('div.tooltip').remove();
-            // Append tooltip
-            tooltipDiv = d3.select('body').append('div').attr('class', 'tooltip');
-            var absoluteMousePos = d3.mouse(bodyNode);
-	    const displayStyle = enabler() ? 'block' : 'none';
-            tooltipDiv.style('left', (absoluteMousePos[0] + 10)+'px')
-                .style('top', (absoluteMousePos[1] - 15)+'px')
-                .style('position', 'absolute')
-                .style('z-index', 1001)
-		.style('pointer-events', 'none')
-		.style('display', displayStyle);
-        })
-        .on('mousemove', function(d, i) {
-            // Move tooltip
-            var absoluteMousePos = d3.mouse(bodyNode);
-            tooltipDiv.style('left', (absoluteMousePos[0] + 10)+'px')
-                .style('top', (absoluteMousePos[1] - 15)+'px');
-            var tooltipText = accessor(d, i) || '';
-            tooltipDiv.html(tooltipText);
-        })
-        .on("mouseout", function(d, i){
-            // Remove tooltip
-            tooltipDiv.remove();
-        });
-
-    };
+d3.helper.tooltip = function (accessor, enabler) {
+  return function (selection) {
+    var tooltipDiv;
+    var bodyNode = d3.select('body').node();
+    selection
+      .on('mouseover', function (d, i) {
+        // Clean up lost tooltips
+        d3.select('body').selectAll('div.tooltip').remove();
+        // Append tooltip
+        tooltipDiv = d3.select('body').append('div').attr('class', 'tooltip');
+        var absoluteMousePos = d3.mouse(bodyNode);
+        const displayStyle = enabler() ? 'block' : 'none';
+        tooltipDiv
+          .style('left', absoluteMousePos[0] + 10 + 'px')
+          .style('top', absoluteMousePos[1] - 15 + 'px')
+          .style('position', 'absolute')
+          .style('z-index', 1001)
+          .style('pointer-events', 'none')
+          .style('display', displayStyle);
+      })
+      .on('mousemove', function (d, i) {
+        // Move tooltip
+        var absoluteMousePos = d3.mouse(bodyNode);
+        tooltipDiv
+          .style('left', absoluteMousePos[0] + 10 + 'px')
+          .style('top', absoluteMousePos[1] - 15 + 'px');
+        var tooltipText = accessor(d, i) || '';
+        tooltipDiv.html(tooltipText);
+      })
+      .on('mouseout', function (d, i) {
+        // Remove tooltip
+        tooltipDiv.remove();
+      });
+  };
 };
